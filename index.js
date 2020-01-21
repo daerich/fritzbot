4713ba143ed2eec17e970bb221b37aa71fc1e0ab
DaErich <fakefakefans@gmail.com>
2020-01-21 20:59:47 +0100

    dead code

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const janitor = require('./janitorclass.js');
 const Janitor = new janitor.Janitor();
 const online = require('./whosonline.js');
 const Online = new online.Online();
+var configval = JSON.parse(fs.readFileSync("inc/config.json"));
 
 
 client.on("ready", () => {
@@ -24,24 +25,28 @@ client.on('message', msg => {
   if (msg == "!presence"){
     Online.whosPlaying(msg);
   }
-
-  if (msg.content.includes("!addGame")){
+  if (msg.channel.id = configval.permchan){
+    Online.setPerms(msg);
+  }
+/*  if (msg.content.includes("!addGame")){
     Online.addGames(msg);
+  } */
+  //debug
+  if (msg == "!id"){
+    console.log(msg.channel.id);
+    console.log(msg.channel.guild.id);
   }
 else {
   Janitor.cleanup(msg);
 }
-//debug
-if (msg == "!id"){
-  console.log(msg.channel.id);
-  console.log(msg.channel.guild.id);
-}
+
+
 });
-client.on("presenceUpdate", (oldUser, newUser) =>{
-   if(oldUser.user.bot == false){
+/*client.on("presenceUpdate", (oldUser, newUser) =>{
+   if(!oldUser.user.bot){
       Online.whosPlaying(oldUser, newUser);
     }
-})
+}); */
 
 //loggin in
 var contents = fs.readFileSync("inc/config.txt");
--- whosonline.js
+++ whosonline.js
@@ -9,20 +9,19 @@ var configval = JSON.parse(fs.readFileSync("inc/config.json"));
 class online{
   constructor(oldUser, newUser, msg){
     //var GamesList = configval.games;
-  /*  this.addGames = function(msg){
-      let contentInsert = msg.content.split(" ")[1];
-      let
-      JSON.stringify()
-    } */
+
+    this.setPerms = function(msg){
+        let cmdlst = msg.content.split(" ");
+        cmd
+    }
     this.whosPlaying = function(oldUser, newUser){
       let oldPresence = oldUser.presence;
       let newPresence = newUser.presence;
       let AnnounceRole = configval.annrole;
       let myGuild = newUser.guild;
-      if(newPresence.game.type == 0 && oldPresence.game.type !== 0){
+        if(newPresence.game.assets.largeText !== oldPresence.game.assets.largeText){
       var AnnounceChannel = myGuild.channels.get(configval.channel);
-      AnnounceChannel.send( '@' + AnnounceRole + ' ' + newUser.displayName + ' just started playing ' + newPresence.game.name);
+      AnnounceChannel.send( '@' + AnnounceRole + ' ' + newUser.displayName + ' just started playing ' + newPresence.assets.largeText + '!');
     }
-
 }}}
 exports.Online = online;
